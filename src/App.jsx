8d6cef98da2515a778240d7d4df424c324d6da23
ronit5036m <ronitshakhari2005@gmail.com>
2026-01-04 10:03:32 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import useAuthStore from "./stores/useAuthStore";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import { ThemeProvider } from "./providers/ThemeProvider";
@@ -33,21 +32,3 @@ const App = () => {
 };
 
 export default App;
-
-// import React from 'react';
-// import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-// import MainLayout from './components/layout/MainLayout';
-// import LandingPage from './pages/LandingPage';
-// import CreateRoom from './pages/CreateRoom';
-// import Lobby from './pages/Lobby';
-// // import Game from './pages/Game'; // Implied existence
-
-// const App = () => {
-//   return (
-//     <BrowserRouter>
-
-//     </BrowserRouter>
-//   );
-// };
-
-// export default App;
